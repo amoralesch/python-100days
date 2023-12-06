ec4d7d7026b2ad291c8700d33b0197deef243c2c
Antonio M <amoralesch@hotmail.com>
2023-12-06 10:08:55 -0500

    Set same text as in the tutorial

--- d001-band-name/band-name.py
+++ d001-band-name/band-name.py
@@ -3,11 +3,11 @@
 # Learn how to grab input from a user and print text to screen,
 # also how to use variables
 
-print("Band name generator\n")
+print("Welcome to the Band Name Generator.")
 
-city = input("What's the name of the city you were born?\n")
-pet = input("What's the name of a pet you had as a child?\n")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
 
-print("Your new band could be named " + city + " " + pet)
+print("Your band name could be " + city + " " + pet)
 
 input("Press enter to exit.")

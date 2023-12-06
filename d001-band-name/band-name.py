7f8a648858f744cfd605fccb329d43eacf1a6629
Antonio M <amoralesch@hotmail.com>
2023-12-06 00:06:28 -0500

    Complete day 1 exercise

---
+++ d001-band-name/band-name.py
@@ -0,0 +1,13 @@
+# Band name generator
+
+# Learn how to grab input from a user and print text to screen,
+# also how to use variables
+
+print("Band name generator\n")
+
+city = input("What's the name of the city you were born?\n")
+pet = input("What's the name of a pet you had as a child?\n")
+
+print("Your new band could be named " + city + " " + pet)
+
+input("Press enter to exit.")

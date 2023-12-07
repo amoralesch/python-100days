ae2e3a8043603dc83fe8c7830ddab609ed8af407
Antonio M <amoralesch@hotmail.com>
2023-12-06 23:09:08 -0500

    Complete day 2 exercise

--- d001-band-name/band-name.py
+++ d001-band-name/band-name.py
@@ -9,5 +9,3 @@ city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet name?\n")
 
 print("Your band name could be " + city + " " + pet)
-
-input("Press enter to exit.")

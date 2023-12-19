cc45da348a0b0020984a82c4338650ae3a4e670a
amoralesch <58353443+amoralesch@users.noreply.github.com>
2023-12-18 19:57:14 -0500

    Complete day 1 exercise (#1)
    * Complete day 1 exercise
    
    * Set same text as in the tutorial

---
+++ d001-band-name/band-name.py
@@ -0,0 +1,13 @@
+# Band name generator
+
+# Learn how to grab input from a user and print text to screen,
+# also how to use variables
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+
+print("Your band name could be " + city + " " + pet)
+
+input("Press enter to exit.")

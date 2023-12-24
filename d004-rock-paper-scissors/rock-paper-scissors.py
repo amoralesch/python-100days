947dcfcffd36f15b43af9693a92fa1c1faadd808
amoralesch <58353443+amoralesch@users.noreply.github.com>
2023-12-24 16:32:24 -0500

    Complete day 4 exercise (#4)
    * Complete day 1 exercise
    
    * Set same text as in the tutorial
    
    * Complete day 2 exercise
    
    * Complete day 3 exercise
    
    * Complete day 4 exercise
    
    * Check for out-of-bound error

---
+++ d004-rock-paper-scissors/rock-paper-scissors.py
@@ -0,0 +1,53 @@
+# Rock-Paper-Scissors
+
+# Learn about arrays and random numbers
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+options = [rock, paper, scissors]
+
+print("Welcome to Rock-Paper-Scissors.")
+user_option = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+
+if user_option < 0 or user_option > 2:
+    print("You typed a wrong number. You lose.")
+else:
+    print(options[user_option])
+
+    print("Computer chose:")
+    cpu_option = random.randint(0, 2)
+    print(options[cpu_option])
+
+    if user_option == cpu_option:
+        print("It's a draw.")
+    elif (user_option + 1) % 3 == cpu_option:
+        print("You lose.")
+    else:
+        print("You won!")

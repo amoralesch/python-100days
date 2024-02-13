e662f6802aa4b40a9571c48375fb3f276526e5b7
amoralesch <58353443+amoralesch@users.noreply.github.com>
2024-02-12 21:30:41 -0500

    Complete day 8 exercise (#7)
    * Complete day 1 exercise
    
    * Set same text as in the tutorial
    
    * Complete day 2 exercise
    
    * Complete day 3 exercise
    
    * Complete day 4 exercise
    
    * Check for out-of-bound error
    
    * Complete day 5 exercise, simple version
    
    * Fix bug on code
    
    * Complete day 5 exercise, harder version
    
    * Add note for day 6 exercise
    
    * Complete day 7 exercise
    
    * Complete day 8 exercise
    
    * Add validation to user input
    
    * verification function
    
    * verification function
    
    * verification function v2
    
    * Add .DS_Store to list of ignored files
    
    * Remove .DS_Store files
    
    * Use new function to get user inputs
    
    ---------
    
    Co-authored-by: Xinna Wang <xinna1@Xinnas-MacBook-Pro-2.local>

---
+++ d008-caesar-cipher/caesar-cipher.py
@@ -0,0 +1,49 @@
+# Caesar Cipher
+
+# Learn about defining functions with and without parameters, assign
+# parameters by position or by name, default values for parameters, and
+# return values.
+
+from art import logo
+from verify import validChoice, isEncodeOrDecode
+import verify
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar_cipher(starting_text, shift_amount, operation):
+    ending_text = ""
+
+    if operation == "decode":
+        shift_amount *= -1
+
+    for char in starting_text:
+        if char in alphabet:
+            index = alphabet.index(char)
+            new_index = (index + shift_amount) % len(alphabet)
+            ending_text += alphabet[new_index]
+        else:
+            ending_text += char
+
+    print(f"Your {operation}d text is {ending_text}")
+
+print(logo)
+print("Welcome to Caesar Cipher.")
+
+end_program = False
+
+while not end_program:
+    operation = validChoice("Type 'encode' to encode a text, or 'decode' to decode it.\n",
+        isEncodeOrDecode)
+
+    text = input("Write the text.\n").lower()
+    shift = int(input("Type the shift number.\n"))
+
+    caesar_cipher(operation=operation, starting_text=text, shift_amount=shift)
+
+    option = validChoice("Do you want to do it again? Type 'yes' or 'no'\n",
+        verify.isYesOrNo, "Pay attention! That is not good a valid choice.")
+
+    if option == 'no':
+        end_program = True
+        print("Goodbye.")

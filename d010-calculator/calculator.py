5e59216224a7129011920e3d9daa2c745f2a9e3d
amoralesch <58353443+amoralesch@users.noreply.github.com>
2024-03-22 21:25:10 -0400

    Finish day 10 exercise (#11)
    * Finish day 10 exercise
    
    * Add extra method

---
+++ d010-calculator/calculator.py
@@ -0,0 +1,60 @@
+# Calculator
+
+# Learn about functions with and without parameters, functions with return
+# values, and dictionaries.
+
+from art import logo
+import os
+
+def add(first, second):
+    return first + second
+
+def minus(first, second):
+    return first - second
+
+def multiply(first, second):
+    return first * second
+
+def divide(first, second):
+    return first / second
+
+def toPower(first, second):
+    return first ** second
+
+operations = {
+    '+': add,
+    '-': minus,
+    '*': multiply,
+    '/': divide,
+    '**': toPower
+}
+
+finish = False
+
+while not finish:
+    print(logo)
+
+    number_1 = float(input("What's the first number?: "))
+
+    for key in operations:
+        print(key)
+
+    restart = False
+
+    while not restart:
+        op = input("Pick an operation: ")
+
+        number_2 = float(input("What's the next number?: "))
+        result = operations[op](number_1, number_2)
+
+        print(f"{number_1} {op} {number_2} = {result}")
+
+        selection = input(f"Type 'y' to continue calculating with {result}, " +
+            "'n' to start a new calculation, or 'x' to exit: ").lower()
+
+        finish = selection == 'x'
+        restart = selection == 'n' or finish
+        number_1 = result
+
+    if not finish:
+        os.system('clear')

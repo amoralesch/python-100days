58149d4df2ebbc1a669252e41141f578b2b1356f
Xinna Wang <xinna1@xinnas-mbp-2.mynetworksettings.com>
2024-03-23 20:09:05 -0400

    feature/day-11 Game

---
+++ d011-blackjack/Untitled-1.py
@@ -0,0 +1,127 @@
+#initially, randomly distribute two cards each to house and player 
+#if over 21, lose | if tie, house win | greater point wins otherwise
+#house has less than 16 points, add one card
+#player gets two cards, reveal
+#ask player if want another card
+#
+
+import random 
+card = ['A',
+         2,
+         3,
+         4,
+         5,
+         6,
+         7,
+         8,
+         9,
+        10,
+       'J',
+       'Q',
+       'K']
+    
+house_hand = []
+player_hand = []
+#random.shuffle(card)
+
+house_hand.append(random.choice(card))
+player_hand.append(random.choice(card))
+house_hand.append(random.choice(card))
+player_hand.append(random.choice(card))
+#print(house_hand)
+#print(player_hand)
+#print(player_hand[0])
+
+house_value = 0
+player_value = 0
+
+def Card_Value(card):
+    if card == 'A':
+        value = 11
+    elif card == 'J' or card == 'Q' or card == 'K':
+        value = 10
+    else:
+        value = card
+    return value
+
+def YesOrNo(user_choice):
+    return user_choice.lower() == "yes" or user_choice.lower() == "no"
+
+def AddCard(user_input, verify_input, invalid_input):
+    additional_card = input(user_input)
+    
+    while not verify_input(additional_card):
+        print(invalid_input)
+        additional_card = input(user_input)
+    return additional_card
+
+def AceValue(user_hand, user_card_value):
+    if ('A' in user_hand) and (user_card_value > 21):
+        user_card_value -= 10
+    return user_card_value
+
+#print(house_hand[len(house_hand)-1])
+house_value = Card_Value(house_hand[0]) + Card_Value(house_hand[len(house_hand)-1])
+player_value = Card_Value(player_hand[0]) + Card_Value(player_hand[len(player_hand)-1])
+player_value = AceValue(player_hand, player_value)
+house_value = AceValue(house_hand, house_value)
+#print(f"House: {house_hand} | {house_value}")
+print(f"Player: {player_hand} | {player_value}")
+
+##### 'A' = 11
+#if (11 in player_hand[]) and (player_value > 21):
+#    player_hand.remove(11)
+#    player_hand.append(1)
+
+
+#Player's choice of adding cards
+while player_value < 21:
+    adding_card = AddCard("Do you want another card? 'yes' or 'no'\n", YesOrNo, "Invalid input, please try again.")
+    adding_card = adding_card.lower() == 'yes'    
+    if adding_card:
+        player_hand.append(random.choice(card))
+        player_value = player_value + Card_Value(player_hand[len(player_hand)-1])
+        if AceValue(player_hand, player_value):
+            player_value = player_value
+        print(f"Player: {player_hand} | {player_value}")
+    else:
+        break
+
+
+while (house_value <= 16):
+    house_hand.append(random.choice(card))
+    house_value = house_value + Card_Value(house_hand[len(house_hand)-1])
+    if AceValue(house_hand, house_value):
+        house_value = house_value
+    print(f"House: {house_hand} | {house_value}")
+      
+    
+    
+    
+print("-------------Game result-------------------")
+if house_value == 21:
+    print(f"House has {house_hand} of {house_value} points")
+    print(f"Player has {player_hand} of {player_value} points")
+    print("House wins")
+elif player_value == 21:
+    print(f"House has {house_hand} of {house_value} points")
+    print(f"Player has {player_hand} of {player_value} points")
+    print("Player wins")
+elif player_value > 21:
+    print(f"House has {house_hand} of {house_value} points")
+    print(f"Player has {player_hand} of {player_value} points")
+    print("Player bust. House wins") 
+elif house_value > 21:
+    print(f"House has {house_hand} of {house_value} points")
+    print(f"Player has {player_hand} of {player_value} points")
+    print("House bust. Player wins")
+elif house_value == player_value:
+    print("Game tie.")
+elif house_value > player_value:
+    print(f"House has {house_hand} of {house_value} points")
+    print(f"Player has {player_hand} of {player_value} points")
+    print("House wins.")
+else:
+    print(f"House has {house_hand} of {house_value} points")
+    print(f"Player has {player_hand} of {player_value} points")
+    print("Player wins.")

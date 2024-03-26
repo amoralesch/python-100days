933eed8b216c38b0bcc3c333b1746513569ebc80
Xinna Wang <xinna1@xinnas-mbp-2.mynetworksettings.com>
2024-03-25 20:39:57 -0400

    adding an extra hard mode for the guessing game

--- d010-d020/d012-guess-my-number/guess-number.py
+++ d010-d020/d012-guess-my-number/guess-number.py
@@ -2,10 +2,10 @@
 
 # Learn about scopes for variables and functions.
 
-def is_easy_or_hard(input):
+def is_easy_or_hard_orAdvanceDifficulty(input):
     val = input.strip().lower()
 
-    return val == 'easy' or val == 'hard'
+    return val == 'easy' or val == 'hard' or val == 'x-hard'
 
 from art import logo
 from helpers import is_integer, ask_input
@@ -16,17 +16,32 @@ print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
 number = random.choice(range(1, 100))
+#print(f"number: {number}")
 
-difficulty = ask_input("Choose a difficulty. Type 'easy' or 'hard': ", is_easy_or_hard)
+difficulty = ask_input("Choose a difficulty. Type 'easy' or 'hard' or 'x-hard': ", is_easy_or_hard_orAdvanceDifficulty)
 attempts = 10
 
-if difficulty == 'hard':
+if difficulty == 'hard' or difficulty == 'x-hard':
     attempts = 5
 
 guess = -1
 while attempts > 0 and guess != number:
     print(f"You have {attempts} attempts remaining to guess the number.")
     guess = int(ask_input("Make a guess: ", is_integer))
+    
+    if difficulty == 'x-hard' and guess == number:
+        number +=1
+        attempts -= 1
+        print("Too low.")
+#        print(f"number: {number}")
+        continue
+        
+    if difficulty == 'x-hard' and guess == number:
+        number -=1
+        attempts -= 1
+        print("Too high.")
+#        print(f"number: {number}")
+        continue
 
     if guess == number:
         print(f"You got it! The answer was {number}")

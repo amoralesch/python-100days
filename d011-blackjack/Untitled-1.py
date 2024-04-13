79aea9d8b7fc13cca7903ca6cd5563cfec2f27f1
Xinna Wang <xinna1@xinnas-mbp-2.mynetworksettings.com>
2024-04-13 18:40:42 -0400

    Create function to calculate total value of cards in hand and utilize in the gaming logic

--- d011-blackjack/Untitled-1.py
+++ d011-blackjack/Untitled-1.py
@@ -8,16 +8,16 @@
 import random 
 card = ['A',
          2,
- #        3,
- #        4,
- #        5,
- #        6,
- #        7,
- #        8,
- #        9,
- #       10,
- #      'J',
- #      'Q',
+         3,
+         4,
+         5,
+         6,
+         7,
+         8,
+         9,
+        10,
+       'J',
+       'Q',
        'K']
     
 house_hand = []
@@ -55,36 +55,45 @@ def AddCard(user_input, verify_input, invalid_input):
         additional_card = input(user_input)
     return additional_card
 
-def AceValue(user_hand, user_card_value):
-    if ('A' in user_hand) and (user_card_value > 21):
-        user_card_value -= 10
-    return user_card_value
+#def AceValue(user_hand, user_card_value):
+#    if ('A' in user_hand) and (user_card_value > 21):
+#        user_card_value -= 10
+#    return user_card_value
 
-#print(house_hand[len(house_hand)-1])
-house_value = Card_Value(house_hand[0]) + Card_Value(house_hand[len(house_hand)-1])
-player_value = Card_Value(player_hand[0]) + Card_Value(player_hand[len(player_hand)-1])
-player_value = AceValue(player_hand, player_value)
-house_value = AceValue(house_hand, house_value)
-#print(f"House: {house_hand} | {house_value}")
+def TotalValue(card_hand):
+    sum_value = 0
+    ace_count = 0
+    for card in card_hand:
+        sum_value += Card_Value(card)
+        if card == 'A':
+            ace_count +=1
+    while ace_count > 0 and sum_value > 21:
+        sum_value -=10
+        ace_count -=1
+    return sum_value
+
+
+#house_value = Card_Value(house_hand[0]) + Card_Value(house_hand[len(house_hand)-1])
+#player_value = Card_Value(player_hand[0]) + Card_Value(player_hand[len(player_hand)-1])
+player_value = TotalValue(player_hand)
+house_value = TotalValue(house_hand)
 print(f"Player: {player_hand} | {player_value}")
    
 while (player_value < 21):
     adding_card = AddCard("Do you want another card? 'yes' or 'no'\n", YesOrNo, "Invalid input, please try again.")
-    player_hand.append(random.choice(card))
-    player_value = player_value + Card_Value(player_hand[len(player_hand)-1])
-    if player_hand[len(player_hand)-1] == 'A' and player_value > 21:
-        player_value -= 10
-    if player_value < 21:
-        adding_card = adding_card.lower() == 'yes' 
+    if adding_card == adding_card.lower() == 'yes': 
+        player_hand.append(random.choice(card))
+        player_value = TotalValue(player_hand) 
     else:
         break
     print(f"player_hand:{player_hand} | {player_value}")
     
 while (house_value <= 16):
     house_hand.append(random.choice(card))
-    house_value += Card_Value(house_hand[len(house_hand)-1])
-    if house_hand[len(house_hand)-1] == 'A' and house_value > 21:
-        house_value -= 10
+    house_value = TotalValue(house_hand)
+    #house_value += Card_Value(house_hand[len(house_hand)-1])
+    #if house_hand[len(house_hand)-1] == 'A' and house_value > 21:
+     #   house_value -= 10
     print(f"House: {house_hand} | {house_value}")
 
     

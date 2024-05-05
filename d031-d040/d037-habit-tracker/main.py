3a7041c13ad47de9ed393b694346a5b32a78f45d
Antonio M <amoralesch@hotmail.com>
2024-05-05 15:35:30 -0400

    Allow users to create accounts

--- d031-d040/d037-habit-tracker/main.py
+++ d031-d040/d037-habit-tracker/main.py
@@ -5,12 +5,36 @@
 # Lear about other HTTP verbs, `POST, `PUT`, and `DELETE`
 
 from etc.helpers import ask_input
+import random
 import pandas
 import requests
 
 
 def is_valid(selection: str) -> bool:
     return selection.isnumeric() and 0 < int(selection) < 10
+
+
+def valid_username(selection: str) -> bool:
+    """Valid regex: [a-z][a-z0-9-]{1,32}"""
+    valid = selection[0:1].isalpha()
+
+    for char in selection[1:]:
+        valid = valid and char.isalnum()
+
+    return valid and 0 < len(selection) <= 32
+
+
+def new_token() -> str:
+    """Valid regex: [ -~]{8,128}"""
+    minimum = ord(' ')
+    maximum = ord('~')
+    token = ''
+    length = random.randint(8, 128)
+
+    for _ in range(length):
+        token += chr(random.randint(minimum, maximum))
+
+    return token
 
 
 def get_option() -> int:
@@ -39,7 +63,63 @@ def show_accounts() -> None:
         print('No accounts saved.')
 
 
+def username_exists(username: str) -> bool:
+    try:
+        data = pandas.read_csv(CONFIG_FILE)
+
+        return username in [row['NAME'] for _, row in data.iterrows()]
+    except FileNotFoundError:
+        return False
+
+
+def store_account(username: str, token: str) -> None:
+    try:
+        data = pandas.read_csv(CONFIG_FILE)
+        accounts = data.to_dict(orient='records')
+    except FileNotFoundError:
+        accounts = []
+
+    accounts.append({
+        'NAME': username,
+        'TOKEN': token
+    })
+
+    df = pandas.DataFrame(accounts)
+    df.to_csv(CONFIG_FILE, index=False)
+
+
+def create_account() -> None:
+    username = ask_input(
+        "What's the username? [a-z][a-z0-9-]{1,32} ",
+        valid_username)
+
+    if username_exists(username):
+        print('That username already exists.')
+
+        return
+
+    token = new_token()
+
+    params = PIXELA_USER_PARAMS
+    params['token'] = token
+    params['username'] = username
+
+    response = requests.post(url=PIXELA_USER_API_ENDPOINT, json=params)
+    response.raise_for_status()
+
+    store_account(username, token)
+    print(f'Account created, go to: https://pixe.la/@{username}')
+
+
 CONFIG_FILE = 'output/accounts.csv'
+
+PIXELA_USER_API_ENDPOINT = 'https://pixe.la/v1/users'
+PIXELA_USER_PARAMS = {
+    'token': 'token',
+    'username': 'username',
+    'agreeTermsOfService': 'yes',
+    'notMinor': 'yes'
+}
 
 restart = True
 
@@ -48,5 +128,7 @@ while restart:
 
     if option == 1:
         show_accounts()
+    elif option == 2:
+        create_account()
     else:
         restart = False
